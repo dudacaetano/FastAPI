985fda1385d40d1937152b3ec8155ee534ad3367
dudacaetano <mariaeduardascaetano@gmail.com>
2024-06-17 22:17:18 -0300

    add architecture- controller, model, database

---
+++ database/database.py
@@ -0,0 +1,20 @@
+from typing import List
+from model.item import Item
+
+items: List[Item] =[
+
+    Item(
+        id= 1,
+        titulo= 'compras',
+        prioridade= 'alta',
+        data= '8-6'
+    ),
+    Item(
+        id=2,
+        titulo= 'atividades',
+        prioridade= 'media',
+        data= '20-06'
+    )
+
+    
+]
